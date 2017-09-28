13b4561defc19b9387708afc0d1dc480eef57c44
mxd1365 <mike.dapiran@gmail.com>
2017-09-27 21:00:49 -0400

    Now you can fly around

---
+++ SlingballProto/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed = 3f;
+    public float turnSpeed = 3f;
+    public float maxVel = 100;
+    private Rigidbody rigBody;
+
+    private float pitch;
+    private float yaw;
+
+	void Awake () {
+        rigBody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+
+        pitch = Input.GetAxis("Horizontal");
+        yaw = Input.GetAxis("Vertical");
+
+        rigBody.AddRelativeTorque(yaw * turnSpeed, pitch * turnSpeed, 0);
+
+        if(Input.GetMouseButton(0))
+        {
+            if (rigBody.velocity.magnitude <= maxVel)
+            {
+                rigBody.AddRelativeForce(0, 0, speed);
+            }
+        }
+
+        //Level out
+      
+    }
+}
